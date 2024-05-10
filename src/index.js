6a4d4e73dd594c103f7d194811b4dac68b3da04a
kimgunho <gunho.kim@themobilityone.com>
2024-05-10 11:39:01 +0900

    스타일 설정 및 라우터 설치

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import 'styles/reset.scss';
+import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
 
 reportWebVitals();
